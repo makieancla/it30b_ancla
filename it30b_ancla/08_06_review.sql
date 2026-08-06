1cfa457f8c69e84f304561a7de01a56734639236
Mc Francis Estoque Ancla <anclamcfrancis037@gmail.com>
2026-08-06 08:50:00 +0800

    student table

---
+++ it30b_ancla/08_06_review.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `student` (
+  `student_id` int(11) NOT NULL,
+  `student_first_name` varchar(50) NOT NULL,
+  `student_last_name` varchar(50) NOT NULL,
+  `student_course` varchar(50) NOT NULL
+);
